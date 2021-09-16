7f36cb64c6fe3a346d04de4e8606f86b7426b4b3
Aidan Marlin <aidan.marlin@pm.me>
2021-09-16 19:27:56 +0100

    config updates

--- configuration.nix
+++ configuration.nix
@@ -124,6 +124,7 @@
      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      virtualbox
      vlc
+     wafw00f
      wget
      whois
      wireshark-qt
