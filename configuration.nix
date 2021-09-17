c5ccfd3013aeb5a9c52b45bbef9caae61dc93991
Aidan Marlin <aidan.marlin@pm.me>
2021-09-17 14:54:59 +0100

    updates

--- configuration.nix
+++ configuration.nix
@@ -132,6 +132,7 @@
      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      virtualbox
      vlc
+     w3m
      wafw00f
      wapiti
      wget
