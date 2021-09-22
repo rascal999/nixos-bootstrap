4a278b1ccd8bc5c1a6b6820e693d81660324c275
Aidan Marlin <spam@gmail.com>
2021-09-22 09:30:56 +0100

    updates

--- configuration.nix
+++ configuration.nix
@@ -150,7 +150,6 @@
      twmn
      unzip
      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-     virtualbox
      vlc
      w3m
      wafw00f
