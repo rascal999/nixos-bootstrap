42570e970540b4778953fb7bb92270ddf3d8d3d8
Aidan Marlin <spam@gmail.com>
2021-10-12 20:27:39 +0100

    updates

--- configuration.nix
+++ configuration.nix
@@ -14,12 +14,20 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
   #boot.loader.grub.efiSupport = true;
-  #boot.loader.grub.efiInstallAsRemovable = true;
+  boot.loader.grub.efiInstallAsRemovable = true;
   boot.loader.grub.useOSProber = true;
   #boot.loader.systemd-boot.enable = true;
   #boot.loader.efi.efiSysMountPoint = "/boot/efi";
   # Define on which hard drive you want to install Grub.
   boot.loader.grub.device = "nodev"; # or "nodev" for efi only
+  boot.loader.grub.extraEntries = ''
+    menuentry "Reboot" {
+      reboot
+    }
+    menuentry "Poweroff" {
+      halt
+    }
+  '';
 
   # networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
