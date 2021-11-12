3e68aba4ae079799a0aa0c2723fef4c5a8e844d6
Aidan Marlin <spam@gmail.com>
2021-11-12 10:30:21 +0000

    nixos-unstable channel

--- configuration.nix
+++ configuration.nix
@@ -10,7 +10,7 @@
       ./hardware-configuration.nix
     ];
 
-  boot.kernelPackages = pkgs.linuxPackages_testing;
+  boot.kernelPackages = pkgs.linuxPackages_latest_libre;
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
@@ -300,6 +300,6 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "21.05"; # Did you read the comment?
+  system.stateVersion = "nixos-unstable"; # Did you read the comment?
 
 }
