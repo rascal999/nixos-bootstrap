37d9846b5445253a66b6a3e1bdb692ff385f65b6
Aidan Marlin <spam@gmail.com>
2021-11-15 11:38:37 +0000

    Monitor setup

--- scripts/display-mobile.sh
+++ scripts/display-mobile.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-sway output eDP-1 pos 0 0
+sway output eDP-1 enable
 sway output DP-1 disable
 sway output HDMI-A-1 disable
+
+sway output eDP-1 pos 0 0
--- scripts/display-work.sh
+++ scripts/display-work.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+
 sway output eDP-1 disable
+sway output DP-1 enable
+sway output HDMI-A-1 enable
+
 sway output DP-1 pos 0 0
 sway output HDMI-A-1 pos 3440 0
