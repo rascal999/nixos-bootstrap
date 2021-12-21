9185074df8158ccb8c9431dfaa7b5578b6c0f41c
Aidan Marlin <spam@gmail.com>
2021-12-21 10:47:26 +0000

    bug fix

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -4,7 +4,12 @@ if [[ "$HOSTNAME" == "nixos-galaxy" ]]; then
     exec /run/current-system/sw/bin/swaymsg output HDMI-A-1 pos 0 0 res 2560x1440
     exec /run/current-system/sw/bin/swaymsg output DP-1 pos 2560 0 res 3440x1440
     exec /run/current-system/sw/bin/swaymsg output eDP-1 pos 6000 0 res 1920x1080
-    exec /run/current-system/sw/bin/swaymsg workspace 1 output HDMI-A-1
-    exec /run/current-system/sw/bin/swaymsg workspace 2 output DP-1
-    exec /run/current-system/sw/bin/swaymsg workspace 3 output eDP-1
+    exec /run/current-system/sw/bin/swaymsg workspace 1
+    exec /run/current-system/sw/bin/swaymsg workspace 4
+
+    exec /run/current-system/sw/bin/swaymsg workspace 3
+    exec /run/current-system/sw/bin/swaymsg workspace 1
+
+    exec /run/current-system/sw/bin/swaymsg workspace 4
+    exec /run/current-system/sw/bin/swaymsg workspace 3
 fi
