866161086b8d738cb8271a2779028e2eac00ab4d
Aidan Marlin <spam@gmail.com>
2021-12-21 10:24:47 +0000

    monitors.sh

---
+++ scripts/monitors.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+if [[ "$HOST" == "nixos-galaxy" ]]; then
+    exec /run/current-system/sw/bin/swaymsg output HDMI-A-1 pos 0 0 res 2560x1440
+    exec /run/current-system/sw/bin/swaymsg output DP-1 pos 2560 0 res 3440x1440
+    exec /run/current-system/sw/bin/swaymsg output eDP-1 pos 6000 0 res 1920x1080
+fi
