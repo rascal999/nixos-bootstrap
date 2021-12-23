c4503c277e1a6966934ebe1640d809c2f294c537
Aidan Marlin <spam@gmail.com>
2021-12-23 17:42:46 +0000

    Monitor layout

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-if [[ "$HOSTNAME" == "nixos-rig" ]]; then
-    exec /run/current-system/sw/bin/swaymsg output HDMI-A-1 pos 0 0 res 2560x1440
-    exec /run/current-system/sw/bin/swaymsg output DP-4 pos 2560 0 res 3440x1440
-    exec /run/current-system/sw/bin/swaymsg output DP-2 disable
-    exec /run/current-system/sw/bin/swaymsg output DP-3 disable
+if [[ `hostname` == "nixos-rig" ]]; then
+    /run/current-system/sw/bin/swaymsg output HDMI-A-1 pos 0 0 res 2560x1440
+    /run/current-system/sw/bin/swaymsg output DP-4 pos 2560 0 res 3440x1440
+    /run/current-system/sw/bin/swaymsg output DP-2 disable
+    /run/current-system/sw/bin/swaymsg output DP-3 disable
 fi
