9e39c93acb2251f533a7911ac59b3d70431898d7
Aidan Marlin <spam@gmail.com>
2022-01-04 13:30:19 +0000

    x11 revert

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
 if [[ `hostname` == "nixos-rig" ]]; then
-    /run/current-system/sw/bin/swaymsg output HDMI-A-1 pos 0 0 res 2560x1440
-    /run/current-system/sw/bin/swaymsg output DP-4 pos 2560 0 res 3440x1440
-    /run/current-system/sw/bin/swaymsg output DP-2 disable
-    /run/current-system/sw/bin/swaymsg output DP-3 disable
+    /run/current-system/sw/bin/xrandr --output DP-4 --auto --right-of HDMI-1
 fi
--- scripts/wayvnc.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ "$HOSTNAME" == "nixos-zalman" ]]; then
-    exec /run/current-system/sw/bin/swaymsg create_output
-    exec /run/current-system/sw/bin/swaymsg output HEADLESS-1 pos 0 0 res 2560x1440
-    exec /run/current-system/sw/bin/wayvnc --output=HEADLESS-1 -f 60 &
-fi
