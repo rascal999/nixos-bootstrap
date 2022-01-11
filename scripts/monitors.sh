a9721a06d0ae553e0476e3799a9bd8626aaa6eaa
Aidan Marlin <spam@gmail.com>
2022-01-11 10:10:38 +0000

    monitor setup

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -7,4 +7,8 @@ if [[ `hostname` == "nixos-rig" ]]; then
     /run/current-system/sw/bin/xrandr --output DP-4 --auto --left-of HDMI-0
     # 2
     /run/current-system/sw/bin/xrandr --output USB-C-0 --auto --right-of HDMI-0
+
+    i3-msg 'workspace 1; append_layout /home/user/.config/i3/nixos-rig-workspace-1.json'
+    i3-msg 'workspace 2; append_layout /home/user/.config/i3/nixos-rig-workspace-2.json'
+    i3-msg 'workspace 7; append_layout /home/user/.config/i3/nixos-rig-workspace-7.json'
 fi
