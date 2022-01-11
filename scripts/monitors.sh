7a0bcac11bcdf5a003d753b239ffa5974d7d78c7
Aidan Marlin <spam@gmail.com>
2022-01-11 10:16:28 +0000

    monitor setup

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -11,4 +11,7 @@ if [[ `hostname` == "nixos-rig" ]]; then
     i3-msg 'workspace 1; append_layout /home/user/.config/i3/nixos-rig-workspace-1.json'
     i3-msg 'workspace 2; append_layout /home/user/.config/i3/nixos-rig-workspace-2.json'
     i3-msg 'workspace 7; append_layout /home/user/.config/i3/nixos-rig-workspace-7.json'
+    /run/current-system/sw/bin/obsidian &
+    /run/current-system/sw/bin/firefox -P "Default User" &
+    /run/current-system/sw/bin/slack &
 fi
