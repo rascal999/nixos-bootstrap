7b55f88113abb4dd104f1185d4c1187a937854f8
Aidan Marlin <spam@gmail.com>
2022-01-11 09:47:15 +0000

    monitor setup

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -3,5 +3,8 @@
 sleep 5
 
 if [[ `hostname` == "nixos-rig" ]]; then
-    /run/current-system/sw/bin/xrandr --output DP-4 --auto --right-of HDMI-1
+    # 0
+    /run/current-system/sw/bin/xrandr --output DP-4 --auto --left-of HDMI-0
+    # 2
+    /run/current-system/sw/bin/xrandr --output USB-C-0 --auto --right-of HDMI-0
 fi
