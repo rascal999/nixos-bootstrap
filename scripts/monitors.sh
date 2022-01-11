b8e69034be867855f2dc7883fb63a1489bc81794
Aidan Marlin <spam@gmail.com>
2022-01-11 10:22:45 +0000

    monitor setup

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
-sleep 5
-
 if [[ `hostname` == "nixos-rig" ]]; then
+    sleep 5
+
     # 0
     /run/current-system/sw/bin/xrandr --output DP-4 --auto --left-of HDMI-0
     # 2
     /run/current-system/sw/bin/xrandr --output USB-C-0 --auto --right-of HDMI-0
+
+    sleep 5
+    i3-msg "workspace 1, move workspace to output HDMI-0"
+    i3-msg "workspace 2, move workspace to output USB-C-0"
+    i3-msg "workspace 7, move workspace to output DP-4"
 
     i3-msg 'workspace 1; append_layout /home/user/.config/i3/nixos-rig-workspace-1.json'
     i3-msg 'workspace 2; append_layout /home/user/.config/i3/nixos-rig-workspace-2.json'
