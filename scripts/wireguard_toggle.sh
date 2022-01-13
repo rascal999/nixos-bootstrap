ea234ed49b8fd0d6663101f58f6531320a56f2a0
Aidan Marlin <spam@gmail.com>
2022-01-13 14:41:31 +0000

    WireGuard script changes

--- scripts/wireguard_toggle.sh
+++ scripts/wireguard_toggle.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 STATUS=`nmcli con show --active | grep wg0 | wc -l`
-CONNECTION=`nmcli con show --active | head -2 | grep -v "NAME.*UUID" | gawk '{ print $1 }'
+CONNECTION=`nmcli con show --active | head -2 | grep -v "NAME.*UUID" | gawk '{ print $1 }'`
 
 if [[ "$STATUS" == "1" ]]; then
     nmcli connection down wg0
