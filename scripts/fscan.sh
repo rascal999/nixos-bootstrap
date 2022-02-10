896e8fb3be8602920174e8c2111b865a69dee8a6
Aidan Marlin <spam@gmail.com>
2022-02-10 14:40:25 +0000

    fscan update

--- scripts/fscan.sh
+++ scripts/fscan.sh
@@ -13,7 +13,7 @@ tmux kill-session -t fscan
 tmux new-session -d -s fscan
 tmux split-window -v -p 33 -t fscan:0.0
 tmux split-window -v -p 50 -t fscan:0.0
-tmux send-keys -t fscan:0.0 "d-nmap $IP" Enter
+tmux send-keys -t fscan:0.0 "d-rustscan $IP" Enter
 tmux send-keys -t fscan:0.1 "d-nikto $URL" Enter
 tmux send-keys -t fscan:0.2 "d-dirb $URL" Enter
 tmux attach-session -t fscan
