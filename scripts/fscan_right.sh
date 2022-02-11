c959c8ef8f9e0e07018a350454cba07e53ebe055
Aidan Marlin <spam@gmail.com>
2022-02-11 09:58:18 +0000

    fscan update

--- scripts/fscan_right.sh
+++ scripts/fscan_right.sh
@@ -11,7 +11,6 @@ URL=$2
 
 tmux kill-session -t fscan_right
 tmux new-session -d -s fscan_right
-tmux split-window -v -p 33 -t fscan_right:0.0
 tmux split-window -v -p 50 -t fscan_right:0.0
 tmux send-keys -t fscan_right:0.0 "d-nmap -sU -T5 $IP" Enter
 tmux send-keys -t fscan_right:0.1 "d-nikto $URL" Enter
