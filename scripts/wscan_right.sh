e785fbcf70c85e178d7cd87ec611580a78b1404c
Aidan Marlin <spam@gmail.com>
2022-02-21 17:44:36 +0000

    wscan

--- scripts/wscan_right.sh
+++ scripts/wscan_right.sh
@@ -9,11 +9,12 @@ fi
 IP=$1
 URL=$2
 
-tmux kill-session -t wscan_left
-tmux new-session -d -s wscan_left
-tmux split-window -v -p 20 -t wscan_left:0.0
-tmux split-window -v -p 50 -t wscan_left:0.0
+tmux kill-session -t wscan_right
+tmux new-session -d -s wscan_right
+tmux split-window -v -p 20 -t wscan_right:0.0
+tmux split-window -v -p 50 -t wscan_right:0.0
 tmux send-keys -t wscan_right:0.0 "d-rpcclient $IP" Enter
-tmux send-keys -t wscan_left:0.1 "d-samrdump $IP" Enter
-tmux send-keys -t wscan_left:0.2 "d-rpcdump $IP" Enter
-tmux attach-session -t wscan_left
+tmux send-keys -t wscan_right:0.1 "d-samrdump $IP" Enter
+tmux send-keys -t wscan_right:0.2 "d-smbclient $IP" Enter
+#tmux send-keys -t wscan_right:0.2 "d-rpcdump $IP" Enter
+tmux attach-session -t wscan_right
