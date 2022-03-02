306694191f67b4dcab305a3068035d5dcdd67552
Aidan Marlin <spam@gmail.com>
2022-03-02 08:47:01 +0000

    public-pentesting-reports

--- resources.sh
+++ resources.sh
@@ -255,6 +255,7 @@ git_update https://github.com/enaqx/awesome-pentest.git $HOME/git/pentest-educat
 git_update https://github.com/bayandin/awesome-awesomeness.git $HOME/git/pentest-education/awesome-awesomeness
 git_update https://github.com/trimstray/the-book-of-secret-knowledge.git $HOME/git/pentest-education/the-book-of-secret-knowledge
 git_update https://github.com/Hack-with-Github/Awesome-Hacking.git $HOME/git/pentest-education/Awesome-Hacking
+git_update https://github.com/juliocesarfort/public-pentesting-reports.git $HOME/git/pentest-education/public-pentesting-reports
 
 ###
 ### Tools
