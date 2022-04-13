e13ce42ec10144e52eb2b240e71ca1e804e42ea0
Aidan Marlin <spam@gmail.com>
2022-04-13 14:09:47 +0100

    Galaxy kernel

--- configuration.nix
+++ configuration.nix
@@ -11,8 +11,7 @@
       ./hostname.nix
     ];
 
-  boot.kernelPackages = lib.mkIf (config.networking.hostName == "nixos-rig") pkgs.linuxPackages;
-  boot.kernelPackages = lib.mkIf (config.networking.hostName == "nixos-galaxy") pkgs.linuxPackages_latest;
+  boot.kernelPackages = lib.mkIf (config.networking.hostName == "nixos-rig") pkgs.linuxPackages else pkgs.linuxPackages_latest;
   boot.kernelParams = [ "intel_pstate=active" ];
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
