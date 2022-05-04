ba27e792cd51e7f2c3686f39ac9a114c7066b4c2
Aidan Marlin <spam@gmail.com>
2022-05-04 12:09:13 +0100

    nixos-rig nvidia

--- configuration.nix
+++ configuration.nix
@@ -338,8 +338,7 @@
   # opensnitch
   services.opensnitch.enable = false;
   # nvidia
-  services.xserver.videoDrivers = if (config.networking.hostName == "nixos-rig") then { [ "nvidia" ] } else { ["amdgpu"] };
-  services.xserver.videoDrivers = lib.mkIf (config.networking.hostName == "nixos-rog") [ "amdgpu" ];
+  services.xserver.videoDrivers = lib.mkIf (config.networking.hostName == "nixos-rig") [ "nvidia" ];
   #hardware.nvidia.modesetting.enable = lib.mkIf (config.networking.hostName == "nixos-rog") true;
   #hardware.nvidia.prime = lib.mkIf (config.networking.hostName == "nixos-rog") {
   #  sync.enable = true;
