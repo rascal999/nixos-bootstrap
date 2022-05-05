2e87c14951e6fca5a0958cd2df3e504a19df71b0
Aidan Marlin <spam@gmail.com>
2022-05-05 16:07:17 +0100

    post_install.sh updates

--- post_install.sh
+++ post_install.sh
@@ -18,45 +18,12 @@ mkdir $HOME/asciinema
 sudo mv /etc/nixos/configuration.nix /etc/nixos/configuration.nix.backup
 sudo cp $HOME/git/nixos-bootstrap/configuration.nix /etc/nixos/configuration.nix
 
+sudo mv /etc/nixos/pkgs /etc/nixos/pkgs.backup
+sudo cp -rf $HOME/git/nixos-bootstrap/pkgs/ /etc/nixos/
+
 # twmnc sound
 sudo mkdir -p $HOME/sounds
 sudo cp -rf $HOME/git/nixos-bootstrap/sounds/WAV_Calm.wav $HOME/sounds
 
 # docker swarm
 docker swarm init
-
-echo
-echo "# Firefox"
-echo "## Plugins"
-echo "- BibItNow!"
-echo "- CanvasBlocker"
-echo "- ClearURLs"
-echo "- Dark Reader"
-echo "- Foxy Proxy"
-echo "- LocalCDN"
-echo "- Read Aloud"
-echo "- MetaMask"
-echo "- Privacy Badger"
-echo "- uBlock Origin"
-echo "## Import bookmarks"
-echo
-echo "# Obsidian"
-echo "## Plugins"
-echo "- Advanced Tables"
-echo "- Calendar"
-echo "- Copy button for code blocks"
-echo "- Daily Notes"
-echo "- Day Planner"
-echo "- Dictionary"
-echo "- Editor Syntax Highlight"
-echo "- Kanban"
-echo "- Mind Map"
-echo "- Ozan's Image in Editor Plugin"
-echo "- Paste URL into selection"
-echo "- Recent Files"
-echo "- Spaced Repetition"
-echo "- Templates"
-echo "- Templater"
-echo
-echo "# ProtonMail"
-echo
