f9d51b47243d715697f08bdddda60f505b10321e
Aidan Marlin <spam@gmail.com>
2022-05-05 15:59:22 +0100

    vim-plugins

--- configuration.nix
+++ configuration.nix
@@ -265,7 +265,7 @@
     # manually loadable by calling `:packadd $plugin-name`
     # however, if a Vim plugin has a dependency that is not explicitly listed in
     # opt that dependency will always be added to start to avoid confusion.
-    opt = [ phpCompletion elm-vim ];
+    #opt = [ phpCompletion elm-vim ];
     # To automatically load a plugin when opening a filetype, add vimrc lines like:
     # autocmd FileType php :packadd phpCompletion
   };
